18362c66c9a2c70451f27cfaf33b5366feafbec3
Francesco <1682099+francescostella@users.noreply.github.com>
2022-05-08 17:16:29 +0200

    feat: set app theme style

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { Provider } from 'react-redux';
 import App from './App.js';
 import store from './redux/store';
 
+/**
+ * React App entry point
+ */
 const container = document.getElementById('app');
 const root = createRoot(container);
 root.render(
@@ -11,3 +14,14 @@ root.render(
     <App />
   </Provider>
 );
+
+/**
+ * Detect OS theme color and set the matching App theme color
+ */
+const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+
+if (prefersDarkScheme.matches) {
+  document.documentElement.classList.add('dark-theme');
+} else {
+  document.documentElement.classList.remove('dark-theme');
+}
