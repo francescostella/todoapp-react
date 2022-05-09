fbc66df447e45a7e2e64100bdf0ac0477dddbc91
Francesco <1682099+francescostella@users.noreply.github.com>
2022-05-09 17:40:24 +0200

    TR-3 feat: add basic styles TodoList, TodoItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,17 +1,34 @@
 import React from 'react';
+import './TodoItem.scss';
 
 function TodoItem({ todo }) {
   return (
-    <li className="todo-list-item">
-      <h3 className="todo-list-item__text">{todo.text}</h3>
-      <div className="todo-list-item__container">
-        <button className="todo-list-item__button todo-list-item__button--completed">
-          Mark as Completed
+    <li className="todo-item">
+      <label className="todo-item__label">
+        <input className="todo-item__checkbox" type="checkbox" />
+        <span className="todo-item__checkmark"></span>
+        <span className="todo-item__description todo-item__description--view">
+          {todo.text}
+        </span>
+        <input
+          className="todo-item__description todo-item__description--edit"
+          type="text"
+          defaultValue="{todo.text}"
+        />
+      </label>
+      <div className="todo-item__buttons">
+        <button className="todo-item__button todo-item__button--delete">
+          ❌
         </button>
-        <button className="todo-list-item__button todo-list-item__button--remove">
-          Remove
+        <button className="todo-item__button todo-item__button--edit">
+          ✏️
+        </button>
+        <button className="todo-item__button todo-item__button--fav">
+          <span className="todo-item__star todo-item__star--empty">☆</span>
+          <span className="todo-item__star todo-item__star--filled">★</span>
         </button>
       </div>
+      <button className="todo-item__info">ℹ️</button>
     </li>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TodoItem from './TodoItem';
 import { useSelector } from 'react-redux';
+import './TodoList.scss';
 
 function TodoList() {
   const todos = useSelector((state) => {
