d295bf35498da2668640d17bb71170afedcfcd9d
Francesco <1682099+francescostella@users.noreply.github.com>
2022-05-09 17:06:44 +0200

    TR-1 feat: improve ThemeToggle init logic

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './ThemeToggle.scss';
 
+let isOSDarkThemed; 
+const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+
+if (prefersDarkScheme.matches) {
+  isOSDarkThemed = true;
+} else {
+  isOSDarkThemed = false;
+}
+
 function ThemeToggle() {
-  const toggleTheme = (event) => {
-    event.preventDefault();
-    document.documentElement.classList.toggle('dark-theme');
-  };
+  const [isDarkTheme, setIsDarkTheme] = useState(isOSDarkThemed);
+
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark-theme', isDarkTheme);
+    console.log('useEffect: ', isDarkTheme);
+  }, [isDarkTheme]);
 
   return (
     <div className="theme-toggle">
-      <button className="theme-toggle__button" onClick={toggleTheme}>
+      <button className="theme-toggle__button" onClick={() => setIsDarkTheme(!isDarkTheme)}>
         <span className="theme-toggle__icon theme-toggle__icon--light">☀️</span>
         <span className="theme-toggle__icon theme-toggle__icon--dark">🌚</span>
       </button>
