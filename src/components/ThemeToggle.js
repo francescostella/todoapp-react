ab2a686148c0b539e4766b082157ad5961a48b56
Francesco <1682099+francescostella@users.noreply.github.com>
2022-05-09 14:50:46 +0200

    TR-1 feat: implement ThemeToggle

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './ThemeToggle.scss';
 
 function ThemeToggle() {
+  const toggleTheme = (event) => {
+    event.preventDefault();
+    document.documentElement.classList.toggle('dark-theme');
+  };
+
   return (
     <div className="theme-toggle">
-      <button className="theme-toggle__button">
+      <button className="theme-toggle__button" onClick={toggleTheme}>
         <span className="theme-toggle__icon theme-toggle__icon--light">☀️</span>
         <span className="theme-toggle__icon theme-toggle__icon--dark">🌚</span>
       </button>
