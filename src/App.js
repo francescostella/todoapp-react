bdb339cdf380740b554c45d54736f77144fec40b
Francesco <1682099+francescostella@users.noreply.github.com>
2022-06-18 20:35:49 +0200

    feat: add mock data

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ import TodoList from './components/TodoList';
 import Footer from './components/Footer';
 import './App.scss';
 
+import { useDispatch } from 'react-redux';
+import { addTodo } from './redux/todosSlice';
+
 function App() {
+  // Populate the app with mock data
+  const dispatch = useDispatch();
+  const mockData = [
+    'Clean Daily Planet desk',
+    'Pick up cape from drycleaning',
+    'Clean the batcave',
+  ];
+
+  mockData.forEach(item => dispatch(addTodo({ text: item})))
+
   return (
     <div className="App">
       <Header />
